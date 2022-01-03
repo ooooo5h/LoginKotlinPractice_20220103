f131ca40fd4b6ec7cff4549377187ef6b7f8c9dc
ooooo5h <ooooo5h91@gmail.com>
2022-01-03 16:55:41 +0900

    작업할 내용을 주석으로 남김

--- app/src/main/java/com/neppplus/loginkotlinpractice_20220103/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginkotlinpractice_20220103/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        로그인 버튼이 눌리면 할 일
+
+//        1. 입력한 아이디와 비밀번호가 무엇인지 변수에 담아두자
+
+//        2. id : admin, pw : qwer1234 두 개가 모두 맞다면, 토스트로 "관리자입니다."
+//        => 둘 중 하나라도 틀리면 "로그인 실패" 토스트
     }
 }
